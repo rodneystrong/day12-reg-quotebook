bef002e233be674f40a6499465b77f94c5709778
aleeex <akang2@gmail.com>
2016-08-22 00:06:25 -0600

    created js files and connected app.js to html

---
+++ js/MainCtrl.js
@@ -0,0 +1,5 @@
+angular
+  .module('quotebook')
+  .controller('MainCtrl', function($scope) {
+    $scope.thisAppIsBroken = 'This app is working!';
+  })
---
+++ js/dataService.js
@@ -0,0 +1,5 @@
+angular
+  .module('quotebook')
+  .service('dataService', function() {
+    
+  })
