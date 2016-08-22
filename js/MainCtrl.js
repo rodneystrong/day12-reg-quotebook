5bb7f7f858a31cf95d07e0a498004497fae9580f
aleeex <akang2@gmail.com>
2016-08-22 00:27:35 -0600

    JSONified all the quote data. gonna try to use  to pull data instead of hardcode

--- js/MainCtrl.js
+++ js/MainCtrl.js
@@ -1,5 +1,10 @@
 angular
   .module('quotebook')
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope, $http) {
     $scope.thisAppIsBroken = 'This app is working!';
+
+    $http.get('js/data.json').then(function(data) {
+      $scope.quotes = data;
+    })
+
   })
