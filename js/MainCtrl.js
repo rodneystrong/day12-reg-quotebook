9305a0b114f527295a75ff8ef229e1653deb166f
aleeex <akang2@gmail.com>
2016-08-22 01:06:35 -0600

    added function to add quotes and put ng-model on correct elements in html

--- js/MainCtrl.js
+++ js/MainCtrl.js
@@ -1,10 +1,20 @@
 angular
   .module('quotebook')
-  .controller('MainCtrl', function($scope, $http) {
+  .controller('MainCtrl', function($scope, $http, dataService) {
     $scope.thisAppIsBroken = 'This app is working!';
 
-    $http.get('js/data.json').then(function(data) {
-      $scope.quotes = data;
-    })
+    // $http.get('js/data.json').then(function(data) {
+    //   $scope.quotes = data;
+    // })
+
+    $scope.quotes = dataService.getQuotes();
+
+    $scope.addQuote = function() {
+      var newQuote = {
+        text: $scope.newQuoteText,
+        author: $scope.newQuoteAuthor
+      }
+      dataService.addQuote(newQuote);
+    }
 
   })
